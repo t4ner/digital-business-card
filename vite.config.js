267713b1847da32026a745bc490c72fab7a3b634
Tunahan Çakıl <tunahancakil@Tunahans-MBP.home>
2025-01-08 01:40:36 +0300

    feat for google search

--- vite.config.js
+++ vite.config.js
@@ -3,28 +3,17 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react()
+  ],
   server: {
-    host: true,
-    port: 3001, // This is the port which we will use in docker
-    // Thanks @sergiomoura for the window fix
-    // add the next lines if you're using windows and hot reload doesn't work
-    watch: {
-      usePolling: true,
-    },
-    proxy: {
-      "/ws": {
-        target: "http://localhost:3001",
-        ws: true,
-        changeOrigin: true,
-      },
-    },
-  },
-  preview: {
+    open: true,
+    cors: true,
+    compression: true,
     host: true,
     port: 3001,
-    watch: {
-      usePolling: true,
-    },
+     watch: {
+       usePolling: true
+     }
   },
 });
